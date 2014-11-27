f406f56e57ee129d9d41eb3a60b0abbd365a9ef3
ajai <ajai.sreyas@gmail.com>
2014-11-27 17:19:44 +0530

    modified settings

--- convertor/settings.py
+++ convertor/settings.py
@@ -109,25 +109,3 @@ LOGGING = {
         },
     },
 }
-try:
-    from local_settings import *
-# # Parse database configuration from $DATABASE_URL
-except:
-    import dj_database_url
-    DATABASES['default'] =  dj_database_url.config()
-
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-    # Allow all host headers
-    ALLOWED_HOSTS = ['*']
-
-    # Static asset configuration
-    import os
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATIC_URL = '/static/'
-
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR,'..','static'),
-    )
